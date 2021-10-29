d00f889195ec9a7f16262bc75f3405f171654375
MichalKr89 <91821382+MichalKr89@users.noreply.github.com>
2021-10-29 20:55:04 +0200

    Add files via upload

---
+++ úkol01opravakodu/src/com/engeto/Main.java
@@ -0,0 +1,68 @@
+package com.engeto;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world!"); //Chybějící "" u Hell world není to proměnná ale jen vypsaný text
+//    }
+//   public static void ukol2() {
+//        String uzivatel = "Karel"; //Chybějící "" u textu který se volží do proměnné uzivatel
+//        System.out.println(uzivatel);
+//    }
+
+//    public static void ukol3() {
+//        int pocetLekci = 10; //místo String má být int aby mohlo být zadáno číslo
+//        System.out.println(pocetLekci);
+//    }
+
+//    public static void ukol4() {
+//        var cena = 10.0; //Při psaní desetiného čísla je třeba použít tečku místo čárky
+//        System.out.println(cena);
+//    }
+
+//    public static void ukol5() {
+//        double cena = 0; //Místo double použít BigDecimal aby nedošlo k chybnému zaokrouhlení
+//        for (int i = 0; i < 10; i++) {
+//            cena += 0.1;
+//        }
+//        System.out.println(cena);
+//         Má vypsat "1.0". Ale vypíše "0.9999" !!!
+//    }
+
+//    public static void ukol6() {
+//        BigDecimal cena = 0;
+//        cena = cena=add(BigDecimal.valueOf(0.1));
+//        System.out.println(cena);
+//    }
+//
+//    public static void ukol7() {
+//        String vykonMotoru = "120"; //Datová typ String místo int pro čísla
+//        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
+//    }
+//
+//    public static void ukol8() {
+//        int velikostKosile = 37;
+//        System.out.println("Velikost košile je: "+velikostKosile+" ."); //místo čárek zapsat +názevproměnné+
+//    }
+//
+//    public static void ukol9() {  //chybí static
+//        System.out.println("Metoda *skoro* bez chybičky!");
+//    }
+
+//    public static void main(String[] args) {
+//        System.out.println("Oprav chyby a spusť všechny metody!");
+//        System.out.println("Komentáře odstraníš klávesovou zkratkou <Ctrl>+</> - použij lomítko na numerické klávesnici.");
+//
+//        ukol1();
+//        ukol2();
+//        ukol3();
+//        ukol4();
+//        ukol5();
+//        ukol6();
+//        ukol7();
+//        ukol8();
+//        ukol9();
+//    }
+    }
+
